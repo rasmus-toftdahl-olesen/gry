e1d181efd6132915e7cccf9726a9f9908525423b
Rasmus Toftdahl Olesen <halfdan@halfdans.net>
2018-01-07 13:06:43 +0100

    Modify the standard collect_cpu.sh script to also record hour/minute/day-of-year and uptime in seconds

--- collect_cpu.sh
+++ collect_cpu.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-DATA="`hostname -s`.CPU `cat /proc/loadavg |cut -f1 -d' '`"
 HOST="127.0.0.1"
 
+DATA="`hostname -s`.CPU `cat /proc/loadavg |cut -f1 -d' '`"
+echo -n $DATA | socat - udp-datagram:$HOST:8124
+
+DATA="`hostname -s`.day `date +%j`"
+echo -n $DATA | socat - udp-datagram:$HOST:8124
+
+DATA="`hostname -s`.hour `date +%H`"
+echo -n $DATA | socat - udp-datagram:$HOST:8124
+
+DATA="`hostname -s`.minute `date +%M`"
+echo -n $DATA | socat - udp-datagram:$HOST:8124
+
+DATA="`hostname -s`.uptime `cat /proc/uptime |cut -f1 -d' '`"
 echo -n $DATA | socat - udp-datagram:$HOST:8124
